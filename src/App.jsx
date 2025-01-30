9c460d496c4e812385d752b51fe76705661351d9
Ivan2000-web <vany6408678@gmail.com>
2025-01-30 09:37:01 +0300

    add: props to journal item component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,34 @@ import JournalItem from './components/JournalItem/JournalItem'
 
 function App() {
 
+  const data = [
+    {
+      title: "Горные породы",
+      text: "Горные породы всегда очень ценились во всём мире",
+      date: new Date
+    },
+    {
+      title: "Поход в горы",
+      text: "Большие горы всегда очень ценились во всём мире",
+      date: new Date
+    }
+  ]
+
   return (
     <>
       <h1>Заголовок</h1>
       <p>Какой-то текст</p>
       <Button />
-      <JournalItem />
+      <JournalItem
+        title={data[0].title}
+        text={data[0].text}
+        date={data[0].date}
+      />
+      <JournalItem
+        title={data[1].title}
+        text={data[1].text}
+        date={data[1].date}
+      />
     </>
   )
 }
--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,15 +1,13 @@
 import './JournalItem.css'
 
-function JournalItem() {
-    const title = "Горные породы";
-    const date = new Date;
-    const text = "Горные породы всегда очень ценились во всём мире";
+function JournalItem({ title, text, date }) {
+    const formatedDate = new Intl.DateTimeFormat('ru-RU').format(date)
 
     return (
         <div className="journal-item">
             <h2 className='journal-item__header'>{title}</h2>
             <h2 className='journal-item__body'>
-                <div className='journal-item__date'>{date.toDateString()}</div>
+                <div className='journal-item__date'>{formatedDate}</div>
                 <div className='journal-item__text'>{text}</div>
             </h2>
         </div>
